fbdadc14b25796e0407c2a00b5f2c16f43a0c803
benja <benjabuddy91@gmail.com>
2017-02-15 16:29:48 +0800

    Configure wines routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :wines
+  
+  root 'wines#index'
 end
