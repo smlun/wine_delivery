b142f8f3a765931d90c1456ea12a7ad481e7cc32
benja <benjabuddy91@gmail.com>
2017-02-15 19:48:22 +0800

    Generate Cart controller
    Index, create, new, destroy, and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'cart/new'
+
+  get 'cart/create'
+
+  get 'cart/show'
+
+  get 'cart/destroy'
+
+  get 'cart/index'
+
   resources :wines
   
   root 'wines#index'
