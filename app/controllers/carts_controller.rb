d97a0c26f5216801d89d4a2fa596075f269c0d72
Ming Lun <smlun@msn.com>
2017-02-16 17:05:03 +0800

    Create validation to stop users from accessing other carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,14 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @carts = Cart.all
   end
 
   def show
     @cart = Cart.find(params[:id])
+    if current_user.cart != @cart
+      redirect_to wines_path
+    end
   end
 
   def new
