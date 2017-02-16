2875a7934fc8fba7fcd0024a2808ffb0ba99504f
Ming Lun <smlun@msn.com>
2017-02-16 16:56:12 +0800

    Fix users able to see all carts in database

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,17 +4,14 @@ class OrdersController < ApplicationController
     if current_user.cart.orders.find_by(wine: @wine)
       @order = current_user.cart.orders.find_by(wine: @wine)
       @order.quantity += params[:order][:quantity].to_i
-      if @order.save
-        redirect_to carts_path
-      end
     else
       @order = Order.new()
       @order.wine = @wine
       @order.quantity = params[:order][:quantity]
       @order.cart = current_user.cart
-      if @order.save
-        redirect_to carts_path
-      end
+    end
+    if @order.save
+      redirect_to cart_path(current_user.cart)
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   has_one :cart
 
+  after_create :create_cart_for_user
+
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def create_cart_for_user
+    Cart.create(user: self)
+  end
 end
